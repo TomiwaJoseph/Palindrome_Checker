d9f17a8624d0dfeb79cae48d7cfacf9430adb369
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-01-27 12:08:08 +0100

    Changed to Class View

--- palindrome.py
+++ palindrome.py
@@ -2,19 +2,31 @@ from tkinter import *
 
 root = Tk()
 root.geometry('400x200')
-root.config(bg='#000')
+root.config(bg='#222')
 
-def checker():
-    word = entry.get()
-    if word == word[::-1]:
-        output.config(f'{word} is a palindrome')
 
-Label(text='This program checks if input is a Palindrome', font='candara 14',
-    fg='#fff').pack(pady=(10,5))
-entry = Entry(justify='center',font='poppins 12',width=22)
-entry.pack(pady=(0,5))
-Button(text='Check',font='candara 14').pack(pady=(0,5))
-output = Label(text='Input is a palindrome',font='poppins 16',fg='green')
-output.pack(pady=(0,5))
+class Checker():
+    def __init__(self):
+        Label(text='This program checks if word is a Palindrome', font='candara 14',
+            fg='#fff',bg='#222').pack(pady=(15,5))
+        self.entry = Entry(justify='center',font='poppins 12',width=22)
+        self.entry.pack(pady=(0,5))
+        Button(text='Check',bg='teal',fg='#fff',font='candara 14',
+            command=self.checker).pack(pady=(10,5))
+        self.output = Label(font='poppins 16')
 
-mainloop()
+
+    def checker(self):
+        self.output.pack(pady=(0,5))
+        word = self.entry.get()
+        if word == '':
+            self.output.config(text='Please input something',fg='red')
+        elif word == word[::-1]:
+            self.output.config(text=f'{word} is a palindrome',fg='green')
+        else:
+            self.output.config(text=f'{word} is not a palindrome',fg='red')
+
+
+if __name__ == '__main__':
+    app = Checker()
+    mainloop()
