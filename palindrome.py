2f89752d702db29fc7e1411458aed10e392084e9
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-01-27 10:55:33 +0100

    First Commit

---
+++ palindrome.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('400x200')
+root.config(bg='#000')
+
+def checker():
+    word = entry.get()
+    if word == word[::-1]:
+        print('same')
+
+Label(text='This program checks if input is a Palindrome', font='candara 14',
+    fg='#fff').pack(pady=(10,5))
+entry = Entry(justify='center',font='poppins 12',width=22)
+entry.pack(pady=(0,5))
+Button(text='Check',font='candara 14').pack(pady=(0,5))
+output = Label(text='Input is a palindrome',font='poppins 16',fg='green')
+output.pack(pady=(0,5))
+
+mainloop()
